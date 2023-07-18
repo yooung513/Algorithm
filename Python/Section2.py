bb7dff144c092371ba4dda3a5ba08cdc041a6cac
LDY <yooung513@gmail.com>
2023-07-18 11:12:44 +0900

    Section 2 격자판 회문수 추가

--- Python/Section2.py
+++ Python/Section2.py
@@ -336,6 +336,26 @@ else:
 
 
 # 격자판 회문수
+# 1차 풀이
+a = [list(map(int, input().split())) for _ in range(7)]
+cnt = 0 
+for i in range(7):
+    for j in range(3):
+        for k in range(2):
+            if a[j+k][i] != a[j+4-k][i]:
+                break
+        else:
+            cnt += 1
+
+        for x in range(2):
+            if a[i][j+x] != a[i][j+4-x]:
+                break
+        else:
+            cnt += 1
+
+print(cnt)
+
+# 2차 풀이
 a = [list(map(int, input().split())) for _ in range(7)]
 cnt = 0
 for i in range(3):
@@ -349,5 +369,20 @@ for i in range(3):
                 break
         else:
             cnt += 1
+
+print(cnt)
+
+# 3차 풀이
+a = [list(map(int, input().split())) for _ in range(7)]
+
+cnt = 0
+for i in range(7):
+  for j in range(3):
+    if a[i][j] == a[i][j+4]:
+      if a[i][j+1] == a[i][j+3]:
+        cnt += 1
+    if a[j][i] == a[j+4][i]:
+      if a[j+1][i] == a[j+3][i]:
+        cnt += 1
 
 print(cnt)
