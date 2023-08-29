d66d06651f747b6e7f82ce8e7a69a3b162d3a98a
이다영 <yooung513@gmail.com>
2023-08-29 20:11:45 +0900

    DFS/BFS

--- Python/Section6.py
+++ Python/Section6.py
@@ -823,3 +823,71 @@ if flag == 'Y':     # 전체 다 익은 경우
     
 else:               # 안 익은 토마토가 있는 경우
     print(-1)
+
+
+
+# 사다리 타기
+import sys
+input = sys.stdin.readline
+
+
+def Solve(x, y):
+    global flag
+
+    if x == 9:
+        if ladder[x][y] == 2:
+            flag = 2
+
+    else:
+        chk[x][y] = 1
+        for i in range(3):
+            xx = x + dx[i]
+            yy = y + dy[i]
+            if 0<=xx<10 and 0<=yy<10 and ladder[xx][yy] != 0 and chk[xx][yy] == 0:
+                Solve(xx, yy)
+                break
+
+
+ladder = [list(map(int, input().split())) for _ in range(10)]
+
+#왼쪽 > 오른쪽 > 아래쪽
+dx = [0, 0, 1]
+dy = [-1, 1, 0]
+flag = 0
+
+for i in range(10):
+    chk = [[0]*10 for _ in range(10)]
+
+    if ladder[0][i] == 1:
+        Solve(0, i)
+    
+    if flag == 2:
+        print(i)
+        break
+
+
+# 역으로 가는 경우
+import sys
+input = sys.stdin.readline
+
+
+def Solve(x, y):
+    chk[x][y] = 1
+
+    if x == 0:
+        print(y)
+
+    else:
+        if y-1>=0 and ladder[x][y-1]==1 and chk[x][y-1]==0:
+            Solve(x, y-1)
+        elif y+1<10 and ladder[x][y+1]==1 and chk[x][y+1]==0:
+            Solve(x, y+1)
+        else:
+            Solve(x-1, y)
+
+ladder = [list(map(int, input().split())) for _ in range(10)]
+chk = [[0]*10 for _ in range(10)]
+
+for y in range(10):
+    if ladder[9][y] == 2:
+        Solve(9, y)
