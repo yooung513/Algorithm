2e11123756b8a50cd176777b7e94c31312efe1fa
이다영 <yooung513@gmail.com>
2023-09-15 01:15:47 +0900

    Section7

--- Python/Section7.py
+++ Python/Section7.py
@@ -100,3 +100,61 @@ for i in range(2, n+1):
         res = lis[i]
 
 print(res)
+
+
+
+# 최대 선 연결하기 (LIS 응용)
+# 원리가 최대 증가수열과 동일
+n = int(input())
+arr = list(map(int, input().split()))
+arr.insert(0, 0)
+
+res = [0]*(n+1)
+res[1] = 1
+
+ans = 0
+for i in range(2, n+1):
+    max = 0
+
+    for j in range(i-1, 0, -1):
+        if arr[i] > arr[j] and res[j] > max:
+            max = res[j]
+    
+    res[i] = max + 1
+
+    if res[i] > ans:
+        ans = res[i]
+
+print(ans)
+
+
+
+# 가장 높은 탑 쌓기
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+bricks = list(list(map(int, input().split())) for _ in range(n))
+bricks.sort(key= lambda x : (-x[0], x[1], x[2]))
+
+res = [0]*n
+res[0] = bricks[0][1]
+highest = res[0]
+
+for i in range(1, n):
+    # i의 최대 높이를 저장하기 위한 변수
+    tmp = 0
+
+    # 자기 자신의 앞에서 가져올 수 있는 최대 높이
+    for j in range(i-1, -1, -1):
+        if bricks[i][2] < bricks[j][2] and res[j] > tmp:
+            tmp = res[j]
+
+    # 자기 자신의 최대 높이
+    res[i] = tmp + bricks[i][1]
+
+    # 가장 높이 쌓을 수 있는 탑의 높이
+    if res[i] > highest:
+        highest = res[i]
+
+print(highest)
