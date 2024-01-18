0e439f5983809e4ed57408417ca1090b5b7d4da3
이다영 <yooung513@gmail.com>
2024-01-18 09:14:59 +0900

    이것이 코딩테스트다 그리디

--- Python/Algorithm/3. Greedy.py
+++ Python/Algorithm/3. Greedy.py
@@ -131,3 +131,106 @@ for i in range(1, len(data)):
         result *= num
         
 print(result)
+
+
+
+# 이코테. 볼링공 고르기
+# 볼링공의 개수 n, 공의 최대 무게 m이 공백으로 구분되어 각각 자연수 형태로 주어짐
+# (1 <= n <= 1000, 1 <= m <= 10)
+# 볼링공의 무게 k가 공백으로 구분돼 자연수 형태로 주어짐 (1 <= k <= m)
+
+# 문제 : 볼링공 무게가 다른 것을 고르는 경우의 수를 찾되, 같은 무게이더라도 다른 볼링공으로 간주한다.
+# 출력 값 : 볼링공을 고르는 경우의 수
+# 아이디어 : 값
+
+import sys
+import time
+from itertools import combinations
+
+n, m = map(int, input().split())
+weight = list(map(int, sys.stdin.readline().split()))
+
+# 단순 구현 (for문 2번 돌리기)
+start = time.time()
+
+res = 0 
+for i in range(n):
+    for j in range(i+1, n):
+        if weight[i] != weight[j]:
+            res += 1
+               
+print(res)
+
+end = time.time()
+print(f"{end - start : .10f} sec")
+
+# 조합 사용
+cmb = list(combinations(weight, 2))
+weight = set(weight)
+print(len(cmb)-(n-len(weight)))
+
+
+# 답안 예시
+n, m = map(int, input().split())
+data = list(map(int, input().split()))
+
+# 1부터 10까지의 무게를 담을 수 있는 리스트
+array = [0] * 11
+
+for x in data:
+    # 각 무게에 해당하는 볼링공의 개수 카운트
+    array[x] += 1
+    
+result = 0
+# 1부터 m까지의 각 무게에 대하여 처리
+for i in range(1, m + 1):
+    n -= array[i]               # 무게가 i인 볼링공의 개수 제외 (= A가 선택할 수 있는 개수 제외)
+    result += array[i] * n      # B가 선택하는 경우의 수와 곱하기
+    
+print(result)
+
+
+
+# 이코테. 무지의 먹방 라이브
+# 답안 예시
+from operator import itemgetter
+
+def solution(food_times, k):
+    # if sum(food_times) < k:
+    #     return -1
+    
+    foods = []
+    n = len(food_times)
+    
+    for i in range(n):
+        foods.append((food_times[i], i+1))       # 음식을 먹는 시간과 음식 번호를 순서대로 기입한다.
+        
+    foods.sort()                                 # 음식을 먹는 시간에 따라 값 정렬
+    
+    pretime = 0                                 # 이전 소요 시간
+    for i, food in enumerate(foods):
+        diff = food[0] - pretime                # 해당 번호의 음식을 먹기 위해 필요한 시간 (높이)
+        
+        if diff != 0:                           # 이전 음식을 먹고 현재 음식을 먹기 위해 소요시간이 필요한 경우
+            spend = diff * n                    # 소요 시간 = 음식을 먹기 위해 필요한 시간 (높이) * 음식의 개수 (n)
+            
+            if spend <= k:                      # 소요시간이 남은시간 보다 작은 경우
+                k -= spend
+                pretime = food[0]
+                
+            else:
+                # 남은 음식들을 원래 순서대로 정렬 (itemgetter를 사용해서 원래 인덱스를 바탕으로 정렬)
+                sublist = sorted(foods[i:], key = foods[i:][1])
+                k %= n
+                
+                return sublist[k][1]
+                
+                
+        n -= 1                                  # 음식의 개수 하나씩 제거
+    
+    return -1 
+
+food_times = [3, 1 ,2]
+k = 5
+print(solution(food_times, k))
+
