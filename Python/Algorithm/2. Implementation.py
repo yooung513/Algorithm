c8711294eee1a6aa5526fa9d7c5039d0daf37599
이다영 <yooung513@gmail.com>
2024-01-23 16:25:08 +0900

    알고리즘 문제풀이

--- Python/Algorithm/2. Implementation.py
+++ Python/Algorithm/2. Implementation.py
@@ -330,3 +330,47 @@ for plan in plans:                          # 이동 계획을 하나씩 확인
     
 print(x, y)
     
+    
+    
+# 이코테. 왕실의 나이트
+pos = input()
+col = {'a':1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6, 'g':7, 'h':8}
+
+x = col.get(pos[0])
+y = int(pos[1])
+dx = [-2, -2, 1, -1, 2, 2, -1, 1]
+dy = [1, -1, 2, 2, 1, -1, -2, -2]
+cnt = 0
+for i in range(8):
+    if 0 < x+dx[i] <= 8 and 0 < y+dy[i] <= 8:
+        cnt += 1
+        
+print(cnt)
+
+
+# 답안 예시
+# 현재 나이트의 위치 입력받기
+input_data = input()
+
+row = int(input_data[1])
+column = int(ord(input_data[0])) - int(ord('a')) + 1
+
+# 나이트가 이동할 수 있는 8가지 방향 정의
+steps = [(-2, -1), (-1, -2), (1, -2), (2, -1), (2, 1), (1, 2), (-1, 2), (-2, 1)]
+
+# 8가지 방향에 대하여 각 위치로 이동이 가능한지 확인
+result = 0 
+for step in steps:
+    # 이동하고자 하는 위치 확인
+    next_row = row + step[0]
+    next_column = column + step[1]
+    
+    # 해당 위치로 이동이 가능하다면 카운트 증가
+    if next_row >= 1 and next_row <= 8 and next_column >= 1 and next_column <= 8:
+        result += 1
+        
+print(result)
+
+
+
+# 
